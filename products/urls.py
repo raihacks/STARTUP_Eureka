3921a9728d9d073773fc43b16068b44b9d9049f6
Gargi Birwatkar <gargibirwatkar2005@gmail.com>
2026-08-22 23:13:39 +0530

    done with adding product listing

---
+++ products/urls.py
@@ -0,0 +1,31 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+
+    path(
+        "",
+        views.products_page,
+        name="products_page"
+    ),
+
+    path(
+        "add/",
+        views.add_product,
+        name="add_product"
+    ),
+
+    path(
+        "edit/<int:product_id>/",
+        views.edit_product,
+        name="edit_product"
+    ),
+
+    path(
+        "view/<int:product_id>/",
+        views.view_product,
+        name="view_product"
+    ),
+
+]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,125 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .models import Product
+from .forms import ProductForm
+
+
+@login_required
+def products_page(request):
+
+    products = Product.objects.filter(
+        owner=request.user
+    ).order_by("-created_at")
+
+    return render(
+        request,
+        "products/product.html",
+        {
+            "products": products
+        }
+    )
+
+
+@login_required
+def add_product(request):
+
+    if request.method == "POST":
+
+        form = ProductForm(
+            request.POST,
+            request.FILES
+        )
+
+        if form.is_valid():
+
+            product = form.save(commit=False)
+
+            # VERY IMPORTANT
+            # The user cannot choose the owner.
+            # Django gets it from the logged-in account.
+
+            product.owner = request.user
+
+            product.save()
+
+            return redirect("products_page")
+
+    else:
+
+        form = ProductForm()
+
+    products = Product.objects.filter(
+        owner=request.user
+    )
+
+    return render(
+        request,
+        "products/product.html",
+        {
+            "form": form,
+            "products": products
+        }
+    )
+
+
+@login_required
+def edit_product(request, product_id):
+
+    product = get_object_or_404(
+        Product,
+        id=product_id,
+        owner=request.user
+    )
+
+    if request.method == "POST":
+
+        form = ProductForm(
+            request.POST,
+            request.FILES,
+            instance=product
+        )
+
+        if form.is_valid():
+
+            form.save()
+
+            return redirect("products_page")
+
+    else:
+
+        form = ProductForm(
+            instance=product
+        )
+
+    return render(
+        request,
+        "products/product.html",
+        {
+            "form": form,
+            "products": Product.objects.filter(
+                owner=request.user
+            ),
+            "editing_product": product
+        }
+    )
+
+
+@login_required
+def view_product(request, product_id):
+
+    product = get_object_or_404(
+        Product,
+        id=product_id
+    )
+
+    return render(
+        request,
+        "products/product.html",
+        {
+            "products": Product.objects.filter(
+                owner=request.user
+            ),
+            "selected_product": product
+        }
+    )
